586b89c2d78bbdc1fc35e1420f36193a03cbe2fd
Rabea Müller <rabea.m@live.de>
2022-11-25 13:43:25 +0100

    Initial commit

---
+++ count_lines.sh
@@ -0,0 +1,8 @@
+# Count_lines.sh
+# 2022 - Rabea Müller
+# Count the lines of all TSV file
+# in the current folder.
+
+echo "Count all lines of TSV files"
+wc -l *tsv
+echo "Counting done - have a nice day"
